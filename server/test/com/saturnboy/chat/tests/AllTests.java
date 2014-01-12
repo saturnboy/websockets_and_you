5f980f49a73405402ae0570e98670a5102a29c0d
Justin Shacklette <justin@saturnboy.com>
2014-01-12 00:11:52 -0700

    initial chat server

---
+++ server/test/com/saturnboy/chat/tests/AllTests.java
@@ -0,0 +1,11 @@
+package com.saturnboy.chat.tests;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ ChatRoomTest.class })
+public class AllTests {
+
+}
